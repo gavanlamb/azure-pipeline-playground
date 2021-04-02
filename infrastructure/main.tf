22643ccdee861ffbf49e35717569a697e3f438b3
Gavan Lamb <gavan.jon.lamb@gmail.com>
2021-04-02 16:57:48 +1100

    Add backend and provider

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,16 @@
+provider "aws" {
+  region  = "ap-southeast-2"
+  version = "3.32.0"
+}
+
+terraform {
+  required_version = ">=0.14.9"
+  backend "s3" {
+    key     = "pwa/terraform.tfstate"
+    encrypt = true
+  }
+}
+
 resource "aws_s3_bucket" "bucket" {
   bucket = "pipeline-test-gfljk"
   acl    = "private"
